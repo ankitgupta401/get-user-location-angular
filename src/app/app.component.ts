938cc312474ce2731fe0145279bd7637e07c8b89
Ankit Gupta <ag3831124@gmail.com>
2022-08-28 20:59:50 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'user-location-angular';
+  location:any;
+  locationJs: any;
+  constructor(private commonService:CommonService){
+
+  }
+
+  ngOnInit(){
+    this.commonService.getLocation().subscribe((response)=>{
+      console.log(response);
+      this.location = response;
+    })
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position);
+      this.locationJs = position.coords;
+    })
+  }
 }
---
+++ src/app/common.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private http:HttpClient) { }
+
+
+  getLocation(){
+    return this.http.get('https://ipapi.co/json/');
+  }
+}
